2aaf1cf69c5686a4a8c48a486ea79edc0403a43a
rubenci-clase <ruben.corisi@educa.jcyl.es>
2024-11-21 09:00:19 +0100

    Update Semaforo.java

--- src/ejercicioSemaforo/Semaforo.java
+++ src/ejercicioSemaforo/Semaforo.java
@@ -22,7 +22,7 @@ public class Semaforo {
     		long startTime = System.currentTimeMillis();
     		
     		try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
